7cd0a8300629ff1fe65acea3fedb202350f20a54
AndrewJWin <fastroute66@live.com>
2021-04-23 14:35:37 -0500

    fix2

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-let express = require("express");
-let api_routes = require("./routes/api.js");
-let path = require("path");
+let express = require('express')
+let api_routes = require('./routes/api.js')
+let path = require('path')
 
-let app = express();
+// Create web application
+let app = express() 
 
-// Serve Vue client
-app.use(express.static("./student-sign-in/dist/"));
+let vueClientPath = path.join(__dirname, 'student-sign-in-client', 'dist')
+app.use(express.static(vueClientPath))
 
-app.use(express.json());
+// be able to handle JSON requests, convert data to JavaScript
+app.use(express.json())
 
-app.use("/api", api_routes);
+app.use('/api', api_routes)
 
 app.use(function(req, res, next) {
-    res.status(404).send("Not found");
+    // respond with a 404 to any other requests
+    res.status(404).send('Not found')
 })
 
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send("Internal Server Error");
+app.use(function(err, req, res, next){
+    console.error(err.stack)
+    res.status(500).send('Server error')
 })
 
-let server = app.listen(process.env.PORT || 3000, function(){
-    console.log("Express running on port", server.address().port);
+// Start server running 
+let server = app.listen(process.env.PORT || 3000, function() {
+    console.log('Express server running on port', server.address().port)
 })
